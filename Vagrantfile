9f10a597e74fd939c1ae4384f345b427f19dd289
Robert Lynch <robert.lynch@rackspace.com>
2013-09-30 15:22:19 -0500

    Initial commmit

---
+++ Vagrantfile
@@ -0,0 +1,11 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "precise64"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = "manifests"
+    puppet.manifest_file  = "ros.pp"
+    puppet.module_path = "modules"
+  end
+
+end
---
+++ manifests/ros.pp
@@ -0,0 +1,46 @@
+Exec {
+  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update';
+}
+
+Exec["apt-update"] -> Package <| |>
+
+node default {
+    apt::ppa { "http://packages.ros.org/ros/ubuntu": key => "http://packages.ros.org/ros.key"; }
+
+    package {
+        'wget': ensure => latest;
+        'python-software-properties': ensure => latest;
+        'vim': ensure => latest;
+        'git': ensure => latest;
+        'dpkg': ensure => latest;
+        'ros-hydro-desktop-full':
+            ensure => latest,
+            require => Apt::Ppa['http://packages.ros.org/ros/ubuntu'];
+        'python-rosinstall':
+            ensure => latest,
+            require => Apt::Ppa['http://packages.ros.org/ros/ubuntu'];
+    }
+}
+
+exec {'rosdep-init':
+    command => '/usr/bin/rosdep init',
+    require => Package['ros-hydro-desktop-full'],
+    creates => '/etc/ros/rosdep/sources.list.d/20-default.list';
+}
+
+exec {'rosdep-update':
+    command => '/usr/bin/rosdep update',
+    require => Exec['rosdep-init'];
+}
+
+file {"/home/vagrant/.bashrc":
+  source => "puppet:///modules/user/bashrc",
+  mode   => 644,
+  owner => 'vagrant',
+  group => 'vagrant',
+  require => Package['ros-hydro-desktop-full'];
+}
